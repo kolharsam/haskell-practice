4151f2f2aa4b1447025fbe0f6c477c93459303d1
Sameer Kolhar <kolhar730@gmail.com>
2019-12-18 01:34:10 +0530

    Update Solution 4

--- 4/main.hs
+++ 4/main.hs
@@ -1,34 +1,16 @@
- -- NOTE: problem incomplete. ask for help
-
 rep :: Integer -> Integer -> [Integer] -> [Integer]
 rep li reps res =
     case reps of
         0 -> []
         reps -> rep li (reps - 1) res ++ [li]
 
-res = []
 repeats :: ([Integer], [Integer]) -> [Integer]
-repeats (xs, reps) =
-    case xs of
-        [] -> res
-        (x:xys) -> do
-            res ++ rep x (head reps) []
-            repeats (xys, (drop 1 reps))
-
-
--- printArr :: Show a => [a] -> IO()
--- printArr putStrLn . concat . map show 
+repeats (li, reps) = repeat_arr li reps [] where
+    repeat_arr [] reps res_arr = res_arr
+    repeat_arr li [] res_arr = res_arr
+    repeat_arr (x:xs) (y:ys) res_arr = repeat_arr xs ys (res_arr ++ (rep x y []))
 
 
 main = do
-    print (rep 4 12 [])
     print (repeats ([1, 2, 3], [4, 0, 2]))
-    -- printArr res
-
--- Problem Statement
-
-
--- Write a function repeat : int list * int list -> int list|repeat:intlist*intlist->intlist that given a list of integers
--- and another list of nonnegative integers, repeats the integers in the first list according 
--- to the numbers indicated by the second list. For example: \verb|repeat ([1,2,3], [4,0,3]) = [1,1,1,1,3,3,3]
--- repeat([1,2,3],[4,0,3])=[1,1,1,1,3,3,3].
+    
