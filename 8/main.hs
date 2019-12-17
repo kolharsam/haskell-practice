d96249aa4644f7095d4600f43d6b3a813e722052
Sameer Kolhar <kolhar730@gmail.com>
2019-12-18 01:35:01 +0530

    Update Solution 8

--- 8/main.hs
+++ 8/main.hs
@@ -1,12 +1,12 @@
-split_up :: [Integer] -> ([Integer], [Integer]) -> ([Integer], [Integer])
-split_up li (res_odd, res_even) =
-    case li of
-        [] -> (res_odd, res_even)
-        (x:xs) -> do
-            case x `mod` 2 == 0 of
-                True -> split_up xs (res_odd, (res_even ++ [x]))
-                False -> split_up xs ((res_odd ++ [x]), res_even)
+split_up :: [Integer] -> ([Integer], [Integer])
+split_up li = split_list li ([], []) where
+    split_list [] (res_odd, res_even) = (res_odd, res_even)
+    split_list (x:xs) (res_odd, res_even) = do
+        if even x
+            then split_list xs (res_odd, (res_even ++ [x]))
+            else split_list xs ((res_odd ++ [x]), res_even)
+
 
 
 main = do
-    print (split_up [1,2,3,4,5,6,7,8,9,3453,234,23,62,623,21] ([], []))
+    print (split_up [1,2,3,4,5,6,7,8,9,3453,234,23,62,623,21])
