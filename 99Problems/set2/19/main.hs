b4b90235349fdaa7978574d48e6bfada152c94e7
Sameer Kolhar <kolhar730@gmail.com>
2020-01-02 21:28:07 +0530

    Add Solution 19 & 20

--- 99Problems/set2/19/main.hs
+++ 99Problems/set2/19/main.hs
@@ -1,5 +1,13 @@
 -- Problem Statement
--- Rotate a list N places to the left
+-- 19. Rotate a list N places to the left
+-- 20. Remove the kth element from the list
+
+-- Some more solutions for each at : https://wiki.haskell.org/99_questions/Solutions/20
+-- https://wiki.haskell.org/99_questions/Solutions/19
+
+
+remove' :: Eq a => [a] -> Int -> (a, [a])
+remove' li pos = ((li !! (pos-1)), ((take (pos - 1) li) ++ (drop pos li)))
 
 rotate :: Eq a => [a] -> Int -> [a]
 rotate li num = do
@@ -12,3 +20,5 @@ main :: IO ()
 main = do
   print (rotate [1,2,3,4,5,6,7] 2)
   print (rotate "abcdefghi" (-4))
+  print (remove' [1,2,3,4,5,6] 3)
+  print (remove' "abcd" 2)
